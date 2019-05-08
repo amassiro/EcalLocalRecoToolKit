164e591a39cc9eddd5d9f8d536c37ccdfc4b7548
amassiro <massironi.andrea@gmail.com>
2019-05-08 09:45:57 +0200

    tests

--- test/gpu/raw2digi_ecalonly_patatrack_and_dump_Viktor.py
+++ test/gpu/raw2digi_ecalonly_patatrack_and_dump_Viktor.py
@@ -126,7 +126,7 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True),
 # Files to process
 #-----------------
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10)
     )
 
 process.source = cms.Source(
